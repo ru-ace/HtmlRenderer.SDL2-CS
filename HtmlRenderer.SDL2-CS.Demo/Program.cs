7bfc289aa551884d9c9c8aa5c61e942cc631acf3
ace <ace@ezotop.ru>
2019-02-26 18:58:43 +0300

    Just create solution and setup dependency

---
+++ HtmlRenderer.SDL2-CS.Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlRenderer.SDL2_CS.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SDL2.SDL2_CS_libs_bundle.Init();
+
+        }
+    }
+}
